0ac2eeb270fea6867827a4439344d8477e4638bc
Audran Pillard <pillardaudran@gmail.com>
2023-08-21 17:11:41 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "cats#index"
   resources :cats, only: %i[index]
-  root to: "cats#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
