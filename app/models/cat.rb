8bec45613f01af206189ef03280f7b7169565494
Laura Beauvais <lauralisa.beauvais@gmail.com>
2023-08-21 16:51:53 +0200

    route created + validations

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,2 +1,7 @@
 class Cat < ApplicationRecord
+  has_many :reservations, dependent: :destroy
+  validates :adress, presence: true
+  validates :sex, presence: true
+  validates :breed, presence: true
+  validates :price, presence: true
 end
