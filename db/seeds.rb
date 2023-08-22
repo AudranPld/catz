d43c6a6a4dc3d8a1750665603c45fddc4cbe79e8
Audran Pillard <pillardaudran@gmail.com>
2023-08-22 14:59:43 +0200

    seed built

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-cat1 = { name: 'Mimi', adress: 'Bordeaux', gender: "Female", breed: "Ragdoll", price: 25 }
-cat2 = { name: 'Chouchou', adress: 'Paris', gender: "Male", breed: "British Short Hair", price: 50 }
 
 
-10.times do
-  email = Faker::Internet.email
-  password = Faker::Internet.password(min_length: 8)
-  user = { email: email, password: password }
-  puts user
-  User.create!(user)
+puts "Starting seed"
+
+User.destroy_all()
+Cat.destroy_all()
+Reservation.destroy_all()
+
+rand(10..20).times do
+  user = User.create({ email: Faker::Internet.email, password: Faker::Internet.password(min_length: 8) })
+  rand(1..3).times do
+    cat = Cat.create({
+      name: ["Whiskers", "Fluffy", "Mittens", "Nibbles", "Sprinkles", "Snuggles", "Paws", "Muffin", "Sunny", "Bubbles", "Cheddar", "Peanut", "Cupcake", "Cinnamon", "Gingersnap", "Fudge", "Tootsie", "Popcorn", "Waffles", "Squeaky"].sample    ,
+      adress: Faker::Internet.email,
+      gender: ["male", "female"].sample,
+      breed: ["Siamese", "Persian", "Maine Coon", "Ragdoll", "Bengal", "Sphynx", "Abyssinian", "British Shorthair", "Scottish Fold", "Russian Blue"].sample,
+      price: rand(5..50)
+      })
+
+    rand(1..3).times do
+      dat = Faker::Date.between(from: Date.new(2000, 1, 1), to: Date.today)
+      Reservation.create({
+        starting_date: dat,
+        ending_date: dat,
+        user: user,
+        cat: cat
+      })
+    end
+  end
 end
-
-
-puts 'Created cat'
+puts 'Done'
