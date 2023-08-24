18103ca21bf13c9d95fc628cbef4ec4fd0f747d8
Audran Pillard <pillardaudran@gmail.com>
2023-08-24 15:16:37 +0200

    done

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,11 +1,13 @@
 class Cat < ApplicationRecord
   has_many :reservations, dependent: :destroy
   belongs_to :user
-  validates :adress, presence: true
+  validates :address, presence: true
   validates :gender, presence: true
   validates :breed, presence: true
   validates :price, presence: true
   validates :name, presence: true
   has_one_attached :photo
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   enum gender: { male: 'Male', female: 'Female' }
 end
