02b2c42a7eb6056a9d6cd7edb66793fd6c3c2c98
Laura Beauvais <lauralisa.beauvais@gmail.com>
2023-08-24 15:25:56 +0200

    working on searchbar

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -8,4 +8,11 @@ class Cat < ApplicationRecord
   validates :name, presence: true
   has_one_attached :photo
   enum gender: { male: 'Male', female: 'Female' }
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_breed,
+    against: [ :name, :breed ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
